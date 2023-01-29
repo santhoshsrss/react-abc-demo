e55cb3872d4f694b2d238a009d665964f11a8f7c
santhoshsrss <santhoshsanjaiyan699@gmail.com>
2023-01-29 17:59:40 +0530

    rout

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
 return (
     <div className="App">
       {/* <Counter /> */}
-      {/* <TicTacToe /> */}
-      <MovieList />
+      <TicTacToe />
+      {/* <MovieList /> */}
       {/* <AddColor /> */}
       {/* <Photos /> */}
     </div>
--- src/TicTacToe.jsx
+++ src/TicTacToe.jsx
@@ -1,15 +1,73 @@
+import { useState } from "react";
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 export function TicTacToe() {
+  const { width, height } = useWindowSize()
+  const [board, setboard] = useState(Array(9).fill(null));
+  const [isTurn, setIsTurn] = useState(true)
+  const handclick = (index) => {
+    if(!winner && board[index] === null){
+      const boardCopy = [...board];
+      boardCopy[index] = isTurn? "X": "O";
+      setboard(boardCopy)
+      setIsTurn(!isTurn)
+    }
+
+  }
+  const decidethewinner = (board) => {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (let i = 0; i < lines.length; i++)  {
+        const [a, b, c] = lines[i];
+        if (board[a] !=null && board[a]===board[b] && board[b]===board[c]){
+          console.log("winner", board[a])
+          return board[a];
+        }        
+      }
+      return null;
+  }
+
+  const winner = decidethewinner(board);
+  const restartGame = () =>{
+    setboard(Array(9).fill(null));
+    setIsTurn(true)
+  }
+
   return (
-    <div>
+    <div className="tic-tac-toe">
+    {winner ? <Confetti width={width} height={height} gravity = {0.05} wind ={0.01} /> : null}
       <h2>TicTacToe Game</h2>
-      <GameBox />
+      <div className="board">
+        {board.map((val, index) => 
+        <GameBox val= {val} findindexvalue = {() => handclick(index)}
+        /> )}
+      </div>
+      <Button onClick={restartGame} variant="contained">Restart</Button>
+      {winner ? <p>The winner is: {winner}</p> : null}
     </div>
   );
 }
 
-function GameBox(){
-  const val ="X"
+function GameBox({val, findindexvalue}){
+  // const [val, setval] = useState("")
+  const styles = {
+    color : val === "X" ? "Green" : "Red", 
+  }
   return (
-    <div className = "game-box">{val}</div>
+    <div style={styles} onClick = {findindexvalue} className = "game-box">
+      
+      {val}
+    
+    </div>
   )
 }
